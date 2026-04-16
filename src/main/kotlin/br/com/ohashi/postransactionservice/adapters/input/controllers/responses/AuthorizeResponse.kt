caa2035a7f62be02024201583f3835f5b6497e06
ImOhashi <leonardoohashi.mkt@gmail.com>
2026-04-16 02:26:22 -0300

    feat: add authorize response constructor to authorize transaction result

--- src/main/kotlin/br/com/ohashi/postransactionservice/adapters/input/controllers/responses/AuthorizeResponse.kt
+++ src/main/kotlin/br/com/ohashi/postransactionservice/adapters/input/controllers/responses/AuthorizeResponse.kt
@@ -1,10 +1,18 @@
 package br.com.ohashi.postransactionservice.adapters.input.controllers.responses
 
+import br.com.ohashi.postransactionservice.application.ports.input.authorize.AuthorizeTransactionResult
 import java.math.BigDecimal
 
-data class AuthorizeResponse (
+data class AuthorizeResponse(
     val nsu: String,
     val amount: BigDecimal,
     val terminalId: String,
     val transactionId: String
-)
+) {
+    constructor(authorizeTransactionResult: AuthorizeTransactionResult) : this(
+        nsu = authorizeTransactionResult.nsu,
+        amount = authorizeTransactionResult.amount,
+        terminalId = authorizeTransactionResult.terminalId,
+        transactionId = authorizeTransactionResult.terminalId
+    )
+}
