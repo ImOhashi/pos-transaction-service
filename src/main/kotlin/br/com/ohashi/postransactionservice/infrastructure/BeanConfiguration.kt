e00e78b9d9a0ba12ae706cfb3a578876ea812590
ImOhashi <leonardoohashi.mkt@gmail.com>
2026-04-16 02:25:06 -0300

    feat: add bean configuration file

---
+++ src/main/kotlin/br/com/ohashi/postransactionservice/infrastructure/BeanConfiguration.kt
@@ -0,0 +1,13 @@
+package br.com.ohashi.postransactionservice.infrastructure
+
+import br.com.ohashi.postransactionservice.application.core.usecases.AuthorizeTransactionUseCase
+import br.com.ohashi.postransactionservice.application.ports.input.authorize.AuthorizeTransactionInputPort
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class BeanConfiguration {
+
+    @Bean
+    fun authorizeTransactionUseCase(): AuthorizeTransactionInputPort = AuthorizeTransactionUseCase()
+}
