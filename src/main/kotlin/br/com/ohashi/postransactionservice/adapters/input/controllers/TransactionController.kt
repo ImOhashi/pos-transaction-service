d3c22d02eab7936c2a1ac0c6b8daf8549286fbf0
ImOhashi <leonardoohashi.mkt@gmail.com>
2026-04-16 02:27:45 -0300

    feat: add transaction controller with /authorize endpoint

---
+++ src/main/kotlin/br/com/ohashi/postransactionservice/adapters/input/controllers/TransactionController.kt
@@ -0,0 +1,36 @@
+package br.com.ohashi.postransactionservice.adapters.input.controllers
+
+import br.com.ohashi.postransactionservice.adapters.input.controllers.requests.AuthorizeRequest
+import br.com.ohashi.postransactionservice.adapters.input.controllers.responses.AuthorizeResponse
+import br.com.ohashi.postransactionservice.application.ports.input.authorize.AuthorizeTransactionInputPort
+import br.com.ohashi.postransactionservice.application.ports.input.authorize.AuthorizeTransactionResult
+import br.com.ohashi.postransactionservice.shared.LoggableClass
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping(path = ["/v1/pos/transactions"])
+class TransactionController(
+    private val authorizeTransactionInputPort: AuthorizeTransactionInputPort
+) : LoggableClass() {
+
+    @PostMapping("/authorize")
+    fun authorize(@RequestBody @Valid authorizeRequest: AuthorizeRequest): ResponseEntity<AuthorizeResponse> =
+        withMDC(
+            "nsu" to authorizeRequest.nsu,
+            "terminalId" to authorizeRequest.terminalId
+        ) {
+            logger.info("Receiving a request to authorize")
+            val result: AuthorizeTransactionResult = authorizeTransactionInputPort.authorize(
+                authorizeTransactionCommand = authorizeRequest.toCommand()
+            )
+
+            return ResponseEntity.ok(
+                AuthorizeResponse(authorizeTransactionResult = result)
+            )
+        }
+}
