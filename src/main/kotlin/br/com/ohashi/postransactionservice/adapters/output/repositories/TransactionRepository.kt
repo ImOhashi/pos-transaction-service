b99cec66a1c712808603a13df4a16943d0223da0
ImOhashi <leonardoohashi.mkt@gmail.com>
2026-04-16 01:20:34 -0300

    feat: add transaction repository interface with method to find transaction by terminal id and nsu

---
+++ src/main/kotlin/br/com/ohashi/postransactionservice/adapters/output/repositories/TransactionRepository.kt
@@ -0,0 +1,11 @@
+package br.com.ohashi.postransactionservice.adapters.output.repositories
+
+import br.com.ohashi.postransactionservice.adapters.output.entities.TransactionEntity
+import org.springframework.data.jpa.repository.JpaRepository
+import java.util.Optional
+
+
+interface TransactionRepository : JpaRepository<TransactionEntity, String> {
+
+    fun findByTerminalIdAndNsu(terminalId: String, nsu: String): Optional<TransactionEntity>
+}
